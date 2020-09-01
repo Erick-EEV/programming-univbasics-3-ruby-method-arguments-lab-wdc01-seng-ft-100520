4e88fef645aa747cc1c4647a176980b20c4441c3
Erick Villeda <eeverickeev@gmail.com>
2020-09-01 17:18:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,8 +10,9 @@ def introduction_with_language (name, language)
 puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-
+require 'pry'
 def introduction_with_language_optional (name, language)
+  binding.pry
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
